e7a049574e04eaabce8e5293b09e53adb6c1c5a9
iamidlek <iamidlek@naver.com>
2022-02-25 01:16:45 +0900

    feat: separate routes

---
+++ map-mapbox/server/routes/searchResults.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const router = express.Router();
+const axios = require("axios");
+const url = require("url"); // install 불필요
+
+router.get("/:query", async (req, res) => {
+  try {
+    // api키와 query string 을 함쳐서 붙여주기
+    const params = new URLSearchParams({
+      access_token: process.env.API_KEY,
+      // url qs로 붙어오는 내용들을 다 붙여줌(? 뒤에오는 키=값들)
+      ...url.parse(req.url, ture).query,
+    });
+    const query = req.params.query; // /뒤 파라이터 query 를 추출
+    const { data } = await axios(
+      `https://api.mapbox.com/geocoding/v5/mapbox.places/${query}.json?${params}`
+    );
+    res.status(200).json(data);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+module.exports = router;
