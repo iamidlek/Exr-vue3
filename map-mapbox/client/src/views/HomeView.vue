94472412a370a987ae1e73b113363e86fcdd107e
iamidlek <iamidlek@naver.com>
2022-02-25 14:26:32 +0900

    feat: load map

--- map-mapbox/client/src/views/HomeView.vue
+++ map-mapbox/client/src/views/HomeView.vue
@@ -1,5 +1,55 @@
 <template>
-  <div class="home">
-    <h1 class="text-xl text-red-500">test Tailwind</h1>
+  <div class="h-screen relative">
+    <div id="map" class="h-full z-[1]"></div>
   </div>
 </template>
+
+<script>
+import leaflet from "leaflet";
+import { onMounted, ref } from "vue";
+
+export default {
+  name: "HomeView",
+  setup() {
+    let map;
+    onMounted(() => {
+      // map 초기화
+      map = leaflet.map("map").setView([51.505, -0.09], 10);
+
+      // add tile layer
+      leaflet
+        .tileLayer(
+          `https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=${process.env.VUE_APP_API_KEY}`,
+          {
+            attribution:
+              'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+            maxZoom: 18,
+            id: "mapbox/streets-v11",
+            tileSize: 512,
+            zoomOffset: -1,
+            accessToken: process.env.VUE_APP_API_KEY,
+          }
+        )
+        .addTo(map);
+
+      getGeolocation();
+    });
+
+    const coords = ref(null);
+    const fetchCoords = ref(null);
+    const geoMarker = ref(null);
+
+    const getGeolocation = () => {
+      fetchCoords.value = true;
+      navigator.geolocation.getCurrentPosition(setCoords, getLocError);
+    };
+
+    const setCoords = (pos) => {
+      console.log(pos); //
+    };
+    const getLocError = (err) => {
+      console.log(err);
+    };
+  },
+};
+</script>
