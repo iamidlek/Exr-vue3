4dd46c818639bad21f24763ab69b11963b9d8a03
iamidlek <iamidlek@naver.com>
2022-02-27 19:02:11 +0900

    conf: set HomeView

---
+++ carousel/src/views/HomeView.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="home">
+    <CarouselItem class="carousel">
+      <SlideItem v-for="(slide, index) in carouselSlides" :key="index">
+        <div class="slide-info">
+          <img :src="require(`../assets/${slide}.jpg`)" />
+        </div>
+      </SlideItem>
+    </CarouselItem>
+  </div>
+</template>
+
+<script>
+import CarouselItem from "../components/CarouselItem.vue";
+import SlideItem from "../components/SlideItem.vue";
+
+export default {
+  name: "HomeView",
+  components: { CarouselItem, SlideItem },
+  setup() {
+    const carouselSlides = ["bg-1", "bg-2", "bg-3"];
+
+    return { carouselSlides };
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.carousel {
+  position: relative;
+  max-height: 100vh;
+  height: 100vh;
+  .slide-info {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    max-height: 100%;
+    height: 100%;
+    img {
+      min-width: 100%;
+      height: 100%;
+      object-fit: cover;
+    }
+  }
+}
+</style>
